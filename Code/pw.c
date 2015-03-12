51e2fa55e654299723ee38b9ac4c852f9fd6ef7f
michaelzicc <michaelzicc32@gmail.com>
2015-03-11 18:19:41 -0700

    For Michael's version of the lab 2 part 2 fsm

---
+++ Code/pw.c
@@ -0,0 +1,16 @@
+#include "p24FJ64GA002.h"
+#include "keypad.h"
+#include "timer.h"
+
+#ifndef INITTIMER_H
+#define	INITTIMER_H
+
+//void initTimer1();
+int checkValid();
+
+//void getTimeString(unsigned minutes, unsigned seconds, unsigned FF);
+void delayUs(unsigned int delay);
+
+#endif	/* INITTIMER_H */
+
+
---
+++ Code/pw.h
@@ -0,0 +1,13 @@
+/*
+ * File:   pw.h
+ * Author:
+ *
+ * Created on Friday, March 06, 2015, 01:47:47 PM
+ */
+
+#ifndef PW_H
+#define	PW_H
+
+#endif	
+
+
